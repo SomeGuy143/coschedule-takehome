82857a331a719e2aabd90c1f2433104cbe02ad27
Xander <anpacella77566@gmail.com>
2025-06-23 22:31:45 -0500

    Various frontend changes. Still WIP.

--- frontend/booklist.js
+++ frontend/booklist.js
@@ -1,3 +1,4 @@
+// this populates the best seller list with a default list
 // window.onload = async function () {
 //     try {
 //         const today = new Date();
@@ -14,13 +15,40 @@
 //                                         <p>${data.results.books[i].description}</p>
 //                                     </div>
 //                                 </div>`;
-//             document.getElementById("listBody").innerHTML += bookRowHtml;
+//             document.getElementById("listInnerScrollContainer").innerHTML += bookRowHtml;
 //         }
 //     } catch (error) {
 //         console.error(error);
 //     }
 // }
 
+// event listener for clicking the search button
+document.getElementById("search").addEventListener("click", async function (event) {
+    try {
+        var selectedDateVal = document.getElementById("date").value;
+        var selectedListVal = document.getElementById("list").value;
+
+        var response = await fetch(`http://localhost:8080/nytbooks/${selectedDateVal}/${selectedListVal}`);
+        var data = await response.json();
+
+        document.getElementById("listInnerScrollContainer").innerHTML = "";
+
+        for (var i = 0; i < data.results.books.length; i++) {
+            var bookRowHtml = `<div class="bookRow" data-isbn13="${data.results.books[i].primary_isbn13}">
+                                    <img class="bookCover" src="${data.results.books[i].book_image}" />
+                                    <div>
+                                        <p class="bookTitle">${data.results.books[i].title}</p>
+                                        <p>${data.results.books[i].description}</p>
+                                    </div>
+                                </div>`;
+            document.getElementById("listInnerScrollContainer").innerHTML += bookRowHtml;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// this click event listener splits/unsplits the screen to show or hide the comment section
 document.querySelectorAll(".bookRow").forEach((bookRow) => {
     bookRow.addEventListener("click", function (event) {
         // set background to white for all other book rows
@@ -38,14 +66,32 @@ document.querySelectorAll(".bookRow").forEach((bookRow) => {
         var commentsVisible = !document.getElementById("commentSection").hidden;
 
         if (commentSectionISBN === clickedISBN && commentsVisible) {
+            // remove css classes to split the screen
             document.getElementById("listBody").classList.remove("split", "left");
             document.getElementById("commentSection").classList.remove("split", "right");
+            // remove css classes to add margin to bottom of split screen
+            document.getElementById("listInnerScrollContainer").classList.remove("verticalScrollMargin");
+            document.getElementById("postedCommentsContainer").classList.remove("verticalScrollMargin");
+            
             document.getElementById("commentSection").hidden = true;
         } else {
-            document.getElementById("commentSection").setAttribute("data-isbn13", clickedISBN) // set isbn13 on commentSection so we can tell whether to show or hide it next time a row is clicked
+            // set isbn13 on commentSection so we can tell whether to show or hide it next time a row is clicked
+            document.getElementById("commentSection").setAttribute("data-isbn13", clickedISBN)
+            // add css classes to split the screen
             document.getElementById("listBody").classList.add("split", "left");
             document.getElementById("commentSection").classList.add("split", "right");
+            // add css classes to add margin to bottom of split screen
+            document.getElementById("listInnerScrollContainer").classList.add("verticalScrollMargin");
+            document.getElementById("postedCommentsContainer").classList.add("verticalScrollMargin");
+
             document.getElementById("commentSection").hidden = false;
         }
     })
 });
+
+// template for comment
+// to be inserted into innerHTML of postedCommentsContainer (id) element
+{/* <div class="commentRow">
+        <p class="userName">User Name</p>
+        <p>Comment...</p>
+    </div> */}
